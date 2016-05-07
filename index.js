99be4d725adbbc100edff48e57566af2d0229ce6
Igor Perfilyev <perfilyevhead@gmail.com>
2016-05-07 15:09:31 +0000

    pwf for log, warn, error + different colors

--- index.js
+++ index.js
@@ -1,24 +1,32 @@
-var log = console.Console.prototype.log;
+var log = pwf(console.Console.prototype.log, '\033[34m');
+var warn = pwf(console.Console.prototype.warn, '\x1b[7;93m');
+var error = pwf(console.Console.prototype.error, '\x1b[93;41m');
 
-var pwf = function() {
-  var prepareStackTrace = Error.prepareStackTrace;
-  Error.prepareStackTrace = function(_, stack){ return stack; };
-  var err = new Error;
-  Error.captureStackTrace(err, arguments.callee);
-  var stack = err.stack;
-  Error.prepareStackTrace = prepareStackTrace;
+console.Console.prototype.log = log;
+console.Console.prototype.warn = warn;
+console.Console.prototype.error = error;
+
+console.log = log;
+console.warn = warn;
+console.error = error;
+
+function pwf(log, ansiColor) {
+  return function() {
+    var prepareStackTrace = Error.prepareStackTrace;
+    Error.prepareStackTrace = function(_, stack){ return stack; };
+    var err = new Error;
+    Error.captureStackTrace(err, arguments.callee);
+    var stack = err.stack;
+    Error.prepareStackTrace = prepareStackTrace;
   
-  var args = Array.prototype.slice.call(arguments);
-  var pwfLine = stack[0].getFileName() + ':' + stack[0].getLineNumber();
-  if (this._stdout === process.stdout) {
-    pwfLine = '\033[91m' + pwfLine + '\033[0m';
+    var args = Array.prototype.slice.call(arguments);
+    var pwfLine = stack[0].getFileName() + ':' + stack[0].getLineNumber();
+    if (this._stdout === process.stdout) {
+      pwfLine = ansiColor + pwfLine + '\033[0m';
+    }
+    args.unshift(pwfLine);
+    return log.apply(this,args);
   }
-  args.unshift(pwfLine);
-  return log.apply(this,args);
-};
-
-console.Console.prototype.log = pwf;
-
-console.log = pwf;
+}
 
 module.exports = console;
--- test/index.js
+++ test/index.js
@@ -1,5 +1,18 @@
+require("../index")
+var myConsole = new console.Console(process.stdout, process.stderrs);
 var test = require('ava').test;
 
 test('log', function(t) {
     console.log("test");
+    myConsole.log("test");
+});
+
+test('warn', function(t) {
+    console.warn("warn");
+    myConsole.warn("warn");
+});
+
+test('error', function(t) {
+    console.error("error");
+    myConsole.error("error");
 });
